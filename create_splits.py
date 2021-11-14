db50ca5702a60dd53c49be85bf23564f942e41db
Tim Kristjan Stüven <t.stueven@kruss.de>
2021-11-14 21:31:47 +0100

    feat: Write split function to split the tfrecords in different folders for training and validation (as symlinks), keep already split test (but symlink as well)

--- create_splits.py
+++ create_splits.py
@@ -1,7 +1,7 @@
 import argparse
-import glob
 import os
 import random
+from pathlib import Path
 
 import numpy as np
 
@@ -10,14 +10,46 @@ from utils import get_module_logger
 
 def split(source, destination):
     """
-    Create three splits from the processed records. The files should be moved to new folders in the
-    same directory. This folder should be named train, val and test.
+    Takes data which is already split in train_and_validation, where ony every 10th frame is saved, 
+    and test, where all frames are present in ordner to create a video in the end. Those data will 
+    be symlinked two three new folder, train, val, and test, where the traina nd validation data 
+    are split as well.
 
     args:
         - source [str]: source data directory, contains the processed tf records
-        - destination [str]: destination data directory, contains 3 sub folders: train / val / test
+        - destination [str]: destination data directory, to contain 3 sub folders:
+            train / val / test
     """
-    # TODO: Implement function
+    training_validation_data_fraction_for_traing = 0.8
+
+    source_path = Path(source)
+    training_validation_path = source_path / "training_and_validation"
+    test_path = source_path / "test"
+    train_test_records = [record for record in training_validation_path.glob("*.tfrecord")]
+    test_records = [record for record in test_path.glob("*.tfrecord")]
+
+    random.shuffle(train_test_records)
+    split_index = int(len(train_test_records) * training_validation_data_fraction_for_traing + 0.5)
+    train_records = train_test_records[:split_index]
+    validation_records = train_test_records[split_index:]
+
+    destination_path = Path(destination)
+    dest_train_path = destination_path / "train"
+    dest_val_path = destination_path / "val"
+    dest_test_path = destination_path / "test"
+
+    os.mkdir(dest_train_path)
+    os.mkdir(dest_val_path)
+    os.mkdir(dest_test_path)
+
+    for record in train_records:
+        os.symlink(record, dest_train_path / record.name)
+
+    for record in validation_records:
+        os.symlink(record, dest_val_path / record.name)
+
+    for record in test_records:
+        os.symlink(record, dest_test_path / record.name)
 
 
 if __name__ == "__main__":
